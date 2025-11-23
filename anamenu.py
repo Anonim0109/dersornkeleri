cc842f7274ee0f8c2da4bb964de1a5e7e3ef4166
Anonim0109 <efsoykan@gmail.com>
2025-11-23 15:42:53 +0300

    proje ödev

--- anamenu.py
+++ anamenu.py
@@ -31,4 +31,3 @@ def anamenu():
         
 anamenu()
 
-
--- moduller/oyunlar.py
+++ moduller/oyunlar.py
@@ -2,9 +2,9 @@ def oyunmenu():
     print("╔═════════════════════╗")
     print("║     OYUNLAR         ║")
     print("╠═════════════════════╣")
-    print("║  1-sayı tahmin oyunu║")
-    print("║  2-yılan oyunu      ║")
-    print("║  3-taş kağıt makas  ║")
+    print("║  1-taş kağıt makas  ║")
+    print("║  2-macera oyunu     ║")
+    print("║  3-                 ║")
     print("║  4-                 ║")
     print("║  5-                 ║")
     print("║  6-                 ║")
@@ -16,6 +16,13 @@ def oyunmenu():
     print("║                     ║")
     print("║    Seçimiz nedir?   ║")
     print("╚═════════════════════╝")
+secim = input() 
+if secim =="1":
+    import oyunlar.tasmakas
+    oyunlar.tasmakas.oyunmenu()
+if secim =="2":
+    import oyunlar.macera
+    oyunlar.macera.oyunmenu()
 
     
 
